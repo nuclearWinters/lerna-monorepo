922f8ddf30ab003a0a2a9c7be42298534f5c09c8
nuclearWinters <38480267+nuclearWinters@users.noreply.github.com>
2021-04-05 23:59:24 -0500

    probando lint-staged

--- packages/frontend-courses/src/Options.tsx
+++ packages/frontend-courses/src/Options.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 export const Options = () => {
-  const [, setShow] = useState(true);
+  const [show, setShow] = useState(true);
   useEffect(() => {
-    setShow((state) => !state);
+    setShow(!show);
   }, []);
   return <h1>Hello world React! Hola Hot Reload Working</h1>;
 };
