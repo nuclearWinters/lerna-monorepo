675b69b520b51c8ec4ea18c479f235ad47d08f24
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2021-05-17 17:27:40 -0500

    chore(added timers and packages updated): added timers

--- packages/backend-courses/src/proto/auth_grpc_pb.js
+++ packages/backend-courses/src/proto/auth_grpc_pb.js
@@ -1,42 +1,35 @@
 // GENERATED CODE -- DO NOT EDIT!
 
-"use strict";
-var grpc = require("@grpc/grpc-js");
-var auth_pb = require("./auth_pb.js");
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var auth_pb = require('./auth_pb.js');
 
 function serialize_authPackage_RenewAccessTokenInput(arg) {
   if (!(arg instanceof auth_pb.RenewAccessTokenInput)) {
-    throw new Error(
-      "Expected argument of type authPackage.RenewAccessTokenInput"
-    );
+    throw new Error('Expected argument of type authPackage.RenewAccessTokenInput');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_authPackage_RenewAccessTokenInput(buffer_arg) {
-  return auth_pb.RenewAccessTokenInput.deserializeBinary(
-    new Uint8Array(buffer_arg)
-  );
+  return auth_pb.RenewAccessTokenInput.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 function serialize_authPackage_RenewAccessTokenPayload(arg) {
   if (!(arg instanceof auth_pb.RenewAccessTokenPayload)) {
-    throw new Error(
-      "Expected argument of type authPackage.RenewAccessTokenPayload"
-    );
+    throw new Error('Expected argument of type authPackage.RenewAccessTokenPayload');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_authPackage_RenewAccessTokenPayload(buffer_arg) {
-  return auth_pb.RenewAccessTokenPayload.deserializeBinary(
-    new Uint8Array(buffer_arg)
-  );
+  return auth_pb.RenewAccessTokenPayload.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-var AuthService = (exports.AuthService = {
+
+var AuthService = exports.AuthService = {
   renewAccessToken: {
-    path: "/authPackage.Auth/renewAccessToken",
+    path: '/authPackage.Auth/renewAccessToken',
     requestStream: false,
     responseStream: false,
     requestType: auth_pb.RenewAccessTokenInput,
@@ -46,6 +39,6 @@ var AuthService = (exports.AuthService = {
     responseSerialize: serialize_authPackage_RenewAccessTokenPayload,
     responseDeserialize: deserialize_authPackage_RenewAccessTokenPayload,
   },
-});
+};
 
 exports.AuthClient = grpc.makeGenericClientConstructor(AuthService);
