3c31ae5236d73c07aa9b7e27f196e3a6d407b505
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2021-06-13 20:39:22 -0500

    feat(spinner and npm update): dependencies updted and ne animated spinner

--- packages/frontend-courses/src/components/Spinner.tsx
+++ packages/frontend-courses/src/components/Spinner.tsx
@@ -1,5 +1,47 @@
-import React, { FC } from "react";
+import React, { CSSProperties, FC, useState } from "react";
+import { useSpring, animated } from "react-spring";
+
+const useForceUpdate = () => {
+  const [, setValue] = useState(0);
+  return () => setValue((value) => value + 1);
+};
 
 export const Spinner: FC = () => {
-  return <div>Loading...</div>;
+  const forceUpdate = useForceUpdate();
+  const style = useSpring({
+    to: {
+      height: 40,
+      opacity: 0,
+      width: 40,
+    },
+    from: {
+      border: "10px solid forestgreen",
+      borderRadius: "100%",
+      height: 0,
+      left: "50%",
+      opacity: 1,
+      position: "absolute",
+      top: "50%",
+      transform: "translateX(-50%) translateY(-50%)",
+      width: 0,
+      zIndex: 1,
+    },
+    reset: true,
+    onRest: forceUpdate,
+  });
+  return (
+    <div style={container}>
+      <animated.div style={style} />
+    </div>
+  );
+};
+
+const { container }: { container: CSSProperties } = {
+  container: {
+    padding: "10px 10px",
+    fontSize: "20px",
+    width: 200,
+    position: "relative",
+    alignSelf: "center",
+  },
 };
