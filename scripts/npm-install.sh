1be401c77e950bf07531b76ead7907221f6df54a
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2021-06-19 18:19:27 -0500

    ci(changed cd commands): changed cd commands

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/lerna-monorepo
+cd ~/lerna-monorepo
 npm install
 npm run bootstrap
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/lerna-monorepo
+cd ~/lerna-monorepo
 make up-prod
