e8f636f5e38e0056b40e3d738c6f9a673d6322bc
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-09-11 14:50:54 -0500

    build(using enviroment variables from sudo): pass env variables to sudo command

--- packages/backend-fintech-mongo-cron/scripts/start_server.sh
+++ packages/backend-fintech-mongo-cron/scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/lerna-monorepo/packages/backend-fintech-mongo-cron
-npm run serve
+MONGO_DB=$MONGO_DB KAFKA_ID=$KAFKA_ID NODE_ENV=$NODE_ENV KAFKA=$KAFKA KAFKA_USERNAME=$KAFKA_USERNAME KAFKA_PASSWORD=$KAFKA_PASSWORD npm run serve
