cd86a98d233a1d6e781c280e2d0ea56e0c137790
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-09-11 20:22:29 -0500

    build(fixed script start): checking correct json file

--- packages/backend-fintech-mongo-cron/scripts/start_server.sh
+++ packages/backend-fintech-mongo-cron/scripts/start_server.sh
@@ -3,9 +3,9 @@ cd /home/ec2-user/lerna-monorepo/packages/backend-fintech-mongo-cron
 aws s3 cp s3://lerna-monorepo-secrets/prod/prod.json prod.json
 KAFKA_ID=cron-fintech
 NODE_ENV=production
-MONGO_DB=`sed -n 's/^\s*"MONGO_DB":\s*"\(.*\)",/\1/p' schema.json`
-KAFKA=`sed -n 's/^\s*"KAFKA":\s*"\(.*\)",/\1/p' schema.json`
-KAFKA_USERNAME=`sed -n 's/^\s*"KAFKA_USERNAME":\s*"\(.*\)",/\1/p' schema.json`
-KAFKA_PASSWORD=`sed -n 's/^\s*"KAFKA_PASSWORD":\s*"\(.*\)",/\1/p' schema.json`
+MONGO_DB=`sed -n 's/^\s*"MONGO_DB":\s*"\(.*\)",/\1/p' prod.json`
+KAFKA=`sed -n 's/^\s*"KAFKA":\s*"\(.*\)",/\1/p' prod.json`
+KAFKA_USERNAME=`sed -n 's/^\s*"KAFKA_USERNAME":\s*"\(.*\)",/\1/p' prod.json`
+KAFKA_PASSWORD=`sed -n 's/^\s*"KAFKA_PASSWORD":\s*"\(.*\)",/\1/p' prod.json`
 rm prod.json
 MONGO_DB=$MONGO_DB KAFKA_ID=$KAFKA_ID NODE_ENV=$NODE_ENV KAFKA=$KAFKA KAFKA_USERNAME=$KAFKA_USERNAME KAFKA_PASSWORD=$KAFKA_PASSWORD npm run serve
