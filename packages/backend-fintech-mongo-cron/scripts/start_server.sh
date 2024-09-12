f31d58e999be4747fe6b088544b896c03dd3975b
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-09-11 23:54:24 -0500

    build(do not leave process in a pending state): let codedeploy continue

--- packages/backend-fintech-mongo-cron/scripts/start_server.sh
+++ packages/backend-fintech-mongo-cron/scripts/start_server.sh
@@ -8,4 +8,4 @@ KAFKA=`jq ".KAFKA" prod.json | sed -e 's/^"//' -e 's/"$//'`
 KAFKA_USERNAME=`jq ".KAFKA_USERNAME" prod.json | sed -e 's/^"//' -e 's/"$//'`
 KAFKA_PASSWORD=`jq ".KAFKA_PASSWORD" prod.json | sed -e 's/^"//' -e 's/"$//'`
 rm prod.json
-(MONGO_DB=$MONGO_DB KAFKA_ID=$KAFKA_ID NODE_ENV=$NODE_ENV KAFKA=$KAFKA KAFKA_USERNAME=$KAFKA_USERNAME KAFKA_PASSWORD=$KAFKA_PASSWORD npm run serve&)
+(MONGO_DB=$MONGO_DB KAFKA_ID=$KAFKA_ID NODE_ENV=$NODE_ENV KAFKA=$KAFKA KAFKA_USERNAME=$KAFKA_USERNAME KAFKA_PASSWORD=$KAFKA_PASSWORD npm run serve > /dev/null 2> /dev/null < /dev/null &)
