c72621b79a301f537267814b6f4bbdf68b3123f9
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-09-27 13:33:12 -0500

    build(fixed kafka id name): fixed conf file

--- packages/backend-auth-node/scripts/start_server.sh
+++ packages/backend-auth-node/scripts/start_server.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/lerna-monorepo
 aws s3 cp --recursive s3://lerna-monorepo-secrets/prod/certs ./certs
 cd /home/ec2-user/lerna-monorepo/packages/backend-auth-node
 aws s3 cp s3://lerna-monorepo-secrets/prod/prod.json prod.json
-KAFKA_ID=backend-fintech-mongo
+KAFKA_ID=backend-auth-node
 NODE_ENV=production
 MONGO_DB=`jq ".MONGO_DB" prod.json | sed -e 's/^"//' -e 's/"$//'`
 REDIS=`jq ".REDIS" prod.json | sed -e 's/^"//' -e 's/"$//'`
