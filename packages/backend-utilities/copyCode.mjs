a451f054659a7f1278c23392afab9a877621dc51
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-10-08 19:47:31 -0500

    fix(grpc now working): fixed module error

--- packages/backend-utilities/copyCode.mjs
+++ packages/backend-utilities/copyCode.mjs
@@ -1,4 +1,19 @@
-import fs from "node:fs"
+import fs from "node:fs";
 
-fs.cpSync("./src/protoAuth", "./build/protoAuth", { recursive: true });
-fs.cpSync("./src/protoAccount", "./build/protoAccount", { recursive: true });
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+fs.cpSync(
+  join(__dirname, "src", "protoAuth"),
+  join(__dirname, "build", "protoAuth"),
+  { recursive: true }
+);
+
+fs.cpSync(
+  join(__dirname, "src", "protoAccount"),
+  join(__dirname, "build", "protoAccount"),
+  { recursive: true }
+);
