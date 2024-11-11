18c9d303c3ffb4c6ee9d645777fb2ea62248bd1f
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-11-11 09:16:15 -0500

    ci(running scripts in non deploy folder): avoid deploy error

--- packages/backend-fintech-mongo/scripts/stop_server.sh
+++ packages/backend-fintech-mongo/scripts/stop_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/lerna-monorepo/packages/backend-fintech-mongo-deploy
+cd /home/ec2-user/lerna-monorepo/packages/backend-fintech-mongo
 killall node || echo "No node process to kill"
