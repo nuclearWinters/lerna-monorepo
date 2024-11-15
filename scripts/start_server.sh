e52b058017d9a3e50ba46a9b12dd7529c1a9ce0d
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2024-11-15 10:21:22 -0500

    ci(fixed deploy): changed ci file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/ec2-user/lerna-monorepo
 aws s3 cp --recursive s3://lerna-monorepo-secrets/prod/certs ./certs
 cd /home/ec2-user/lerna-monorepo
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/ec2-user/lerna-monorepo
 killall node || echo "No node process to kill"
